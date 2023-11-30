efe0c0dfd9002bc56ce6320e20ff37e420f3df9e
BianaAldrian <123650902+BianaAldrian@users.noreply.github.com>
2023-11-30 23:32:53 +0800

    updates

--- UMEP 2.0/Assets/Scripts/Database/Register.cs
+++ UMEP 2.0/Assets/Scripts/Database/Register.cs
@@ -11,7 +11,7 @@ public class Register : MonoBehaviour
 
     public string next_scene, user_type;
     public TMP_Text errorHolder;
-    public TMP_InputField id_number, mobile_number, password;
+    public TMP_InputField id_number, mobile_number, password, password2;
     public Button submit;
 
     // Start is called before the first frame update
@@ -23,35 +23,47 @@ public class Register : MonoBehaviour
 
     // Method to check inputs when the submit button is clicked
     void CheckInputs()
+{
+    // Reset error messages
+    errorHolder.text = "";
+
+    // Check if either id_number or mobile_number is empty
+    if (string.IsNullOrEmpty(id_number.text) || string.IsNullOrEmpty(mobile_number.text))
     {
-        // Reset error messages
-        errorHolder.text = "";
-
-        // Check if either id_number or mobile_number is empty
-        if (string.IsNullOrEmpty(id_number.text) || string.IsNullOrEmpty(mobile_number.text))
+        // Display error message in the input field with error color
+        if (string.IsNullOrEmpty(id_number.text))
         {
-            // Display error message in the input field with error color
-            if (string.IsNullOrEmpty(id_number.text))
-            {
-                id_number.placeholder.GetComponent<TextMeshProUGUI>().color = Color.red;
-                id_number.placeholder.GetComponent<TextMeshProUGUI>().text = "ID Number cannot be empty!";
-            }
-
-            if (string.IsNullOrEmpty(mobile_number.text))
-            {
-                mobile_number.placeholder.GetComponent<TextMeshProUGUI>().color = Color.red;
-                mobile_number.placeholder.GetComponent<TextMeshProUGUI>().text = "Mobile Number cannot be empty!";
-            }
-
-            // Display a general error message
-            errorHolder.text = "Please fill in all required fields.";
+            id_number.placeholder.GetComponent<TextMeshProUGUI>().color = Color.red;
+            id_number.placeholder.GetComponent<TextMeshProUGUI>().text = "ID Number cannot be empty!";
         }
-        else
+
+        if (string.IsNullOrEmpty(mobile_number.text))
         {
-            // Inputs are not empty, you can proceed with your registration logic here
-            CheckUser();
+            mobile_number.placeholder.GetComponent<TextMeshProUGUI>().color = Color.red;
+            mobile_number.placeholder.GetComponent<TextMeshProUGUI>().text = "Mobile Number cannot be empty!";
         }
+
+        // Display a general error message
+        errorHolder.text = "Please fill in all required fields.";
     }
+    else if(password.text != password2.text)
+    {
+        // Passwords do not match
+        password.placeholder.GetComponent<TextMeshProUGUI>().color = Color.red;
+        password.placeholder.GetComponent<TextMeshProUGUI>().text = "Passwords do not match!";
+        
+        password2.placeholder.GetComponent<TextMeshProUGUI>().color = Color.red;
+        password2.placeholder.GetComponent<TextMeshProUGUI>().text = "Passwords do not match!";
+
+        // Display a general error message
+        errorHolder.text = "Passwords do not match.";
+    }
+    else
+    {
+        // Inputs are not empty and passwords match, you can proceed with your registration logic here
+        CheckUser();
+    }
+}
 
     void CheckUser()
     {
@@ -67,7 +79,7 @@ public class Register : MonoBehaviour
         if (isConnected)
         {
             Debug.Log("Connected");
-            StartCoroutine(CheckUserCoroutine(serverIP));
+            StartCoroutine(RegisterUserCoroutine(serverIP));
         }
     }
 
@@ -106,8 +118,11 @@ public class Register : MonoBehaviour
     {
         WWWForm form = new WWWForm();
         form.AddField("id_number", id_number.text);
+        form.AddField("mobile_number", mobile_number.text);
+        form.AddField("password", password.text);
+        form.AddField("user_type", user_type);
 
-        UnityWebRequest webRequest = UnityWebRequest.Post($"http://{serverIP}/UMEP/check_user.php", form);
+        UnityWebRequest webRequest = UnityWebRequest.Post($"http://{serverIP}/UMEP/register_user.php", form);
         yield return webRequest.SendWebRequest();
 
         if (webRequest.result != UnityWebRequest.Result.Success)
@@ -121,6 +136,11 @@ public class Register : MonoBehaviour
         {
             string response = webRequest.downloadHandler.text;
             Debug.Log($"Server response: {response}");
+
+            if (response == "exist")
+            {
+                errorHolder.text = "ID already exists! Please try logging in.";
+            }
 
             if (response == "successful")
             {
