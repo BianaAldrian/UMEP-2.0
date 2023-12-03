a050a3f0b7468b3ed0bad7acce221cecab218554
BianaAldrian <123650902+BianaAldrian@users.noreply.github.com>
2023-12-04 03:09:28 +0800

    update

--- UMEP 2.0/Assets/Scripts/Database/Login.cs
+++ UMEP 2.0/Assets/Scripts/Database/Login.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Login : MonoBehaviour
 {
+    public CheckConnection checkConnection; //Calling other script to connect
+
     // Start is called before the first frame update
     void Start()
     {
