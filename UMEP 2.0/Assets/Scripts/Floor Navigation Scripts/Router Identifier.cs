3ab64daf2d98e4c1215a3524c5d2df170f0b71ac
BianaAldrian <123650902+BianaAldrian@users.noreply.github.com>
2023-12-03 13:11:31 +0800

    update

---
+++ UMEP 2.0/Assets/Scripts/Floor Navigation Scripts/Router Identifier.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RouterIdentifier : MonoBehaviour
+{
+    public Button Interactive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!Permission.HasUserAuthorizedPermission(Permission.FineLocation))
+        {
+            Permission.RequestUserPermission(Permission.FineLocation);
+        }
+
+        Interactive.onClick.AddListener(Identify_Router);
+    }
+
+    void Identify_Router()
+    {
+        if (Permission.HasUserAuthorizedPermission(Permission.FineLocation))
+        {
+            using var activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
+            using var wifiManager = activity.Call<AndroidJavaObject>("getSystemService", "wifi");
+            using var wifiInfo = wifiManager.Call<AndroidJavaObject>("getConnectionInfo");
+            string bssid = wifiInfo.Call<string>("getBSSID");
+
+            Debug.Log($"BSSID: {bssid}");
+
+            if (bssid == "00:eb:d8:7c:84:8c")
+            {
+                SceneManager.LoadScene("10th floor");
+            }
+            else if (bssid == "00:eb:d8:c7:88:88")
+            {
+                SceneManager.LoadScene("9th floor");
+            }
+            else
+            {
+                Debug.Log("Invalid Router");
+            }
+        }
+        else
+        {
+            Debug.Log("Fine location permission is not granted.");
+        }
+    }
+}
